679c508f183e07de5a6af7321ee0b1626002a44d
Grzegorz Sowa <grzegorz.sowa314@gmail.com>
2022-05-27 20:09:28 +0200

    add logging in RegisterExchangeService

--- src/main/kotlin/com/safecrack/proxy/register/RegisterExchangeService.kt
+++ src/main/kotlin/com/safecrack/proxy/register/RegisterExchangeService.kt
@@ -1,5 +1,6 @@
 package com.safecrack.proxy.register
 
+import com.safecrack.proxy.logger
 import org.springframework.http.RequestEntity
 import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Component
@@ -13,6 +14,16 @@ class RegisterExchangeService(
         request: RequestEntity<String>,
         response: ResponseEntity<String>
     ) {
+        log.info("Request method: ${request.method}")
+        log.info("Request url: ${request.url}")
+        log.info("Request headers: ${request.headers}")
+        log.info("Request body: ${request.body}")
+        log.info("Response status: ${response.statusCode}")
+        log.info("Response headers: ${response.headers}")
+        log.info("Response body: ${response.body}")
+    }
 
+    companion object {
+        val log by logger()
     }
 }
